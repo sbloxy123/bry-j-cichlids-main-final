d3dc1ac442f8dbe30cf359b31e3185472f36c775
sbloxy123 <sbloxy123@gmail.com>
2021-03-02 22:58:14 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://evening-reaches-18582.herokuapp.com"
+          : "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         contentTypes: [`user`],
         //If using single types place them in this array.
